11420eb0b5b63583da381c813f4c20663611c873
Sebastian Büge <sebastian_buege@gmx.de>
2024-12-10 15:23:15 +0100

    add meta data model service and tests

--- src/test/java/my/photomanager/TestConstants.java
+++ src/test/java/my/photomanager/TestConstants.java
@@ -1,6 +1,27 @@
 package my.photomanager;
 
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
 public class TestConstants {
 	public final static String INTEGRATION_TEST = "INTEGRATION-TEST";
 	public final static String UNIT_TEST = "UNIT-TEST";
+
+	public final static Path TEST_FILE_PATH = Path.of("src", "test", "resources", "TestFiles");
+	public final static Path TEST_PHOPTOS_PATH = TEST_FILE_PATH.resolve("photos");
+
+
+	public static Stream<Path> getPhotoFilesWithMetaData() {
+		return Stream.of(
+				TEST_PHOPTOS_PATH.resolve(Path.of("withMetaData", "Brandenburg_Gate_with_metadata.webp")),
+				TEST_PHOPTOS_PATH.resolve(Path.of("withMetaData", "Brandenburg_Gate_with_metadata.jpg"))
+		);
+	}
+
+	public static Stream<Path> getPhotoFilesWithoutMetaData() {
+		return Stream.of(
+				TEST_PHOPTOS_PATH.resolve(Path.of("withoutMetaData", "Brandenburg_Gate_no_metadata.webp")),
+				TEST_PHOPTOS_PATH.resolve(Path.of("withoutMetaData", "Brandenburg_Gate_no_metadata.jpg"))
+		);
+	}
 }
