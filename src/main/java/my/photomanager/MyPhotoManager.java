83a6c02f51efa5cbaf8812743ece3bf7d30bdf50
Sebastian Büge <sebastian_buege@gmx.de>
2024-12-10 09:26:07 +0100

    add spring project files (#3)

---
+++ src/main/java/my/photomanager/MyPhotoManager.java
@@ -0,0 +1,13 @@
+package my.photomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPhotoManager {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPhotoManager.class, args);
+	}
+
+}
