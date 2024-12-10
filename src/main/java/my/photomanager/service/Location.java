3a6a428b5428531abad72dc8983c8d1e364dd06d
8u363 <74074904+8u363@users.noreply.github.com>
2024-12-10 14:00:34 +0100

    5 add location service and model (#6)
    * add dependencies
    
    * add location, service and tests

---
+++ src/main/java/my/photomanager/service/Location.java
@@ -0,0 +1,36 @@
+package my.photomanager.service;
+
+
+public record Location(String country,
+					   String city,
+					   String postalCode,
+					   String street,
+					   String houseNumber) implements ILocation {
+
+
+
+	@Override
+	public String getCountry() {
+		return country;
+	}
+
+	@Override
+	public String getCity() {
+		return city;
+	}
+
+	@Override
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	@Override
+	public String getStreet() {
+		return street;
+	}
+
+	@Override
+	public String getHouseNumber() {
+		return houseNumber;
+	}
+}
