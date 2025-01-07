3de79d6f235f1f84ce743410d25ec9764e9c7e47
Sebastian Büge <sebastian_buege@gmx.de>
2025-01-07 13:38:06 +0100

    update photo repository

--- src/main/java/my/photomanager/v1/repository/PhotoRepository.java
+++ src/main/java/my/photomanager/v1/repository/PhotoRepository.java
@@ -1,5 +1,9 @@
 package my.photomanager.v1.repository;
 
-public interface PhotoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import my.photomanager.v1.model.Photo;
+
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
 
 }
