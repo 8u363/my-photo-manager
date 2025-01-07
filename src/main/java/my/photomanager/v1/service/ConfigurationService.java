4df1d2342412d93ad15a33fc88f205e2a88f9b9b
Sebastian Büge <sebastian_buege@gmx.de>
2025-01-07 13:38:45 +0100

    update configuration service

--- src/main/java/my/photomanager/v1/service/ConfigurationService.java
+++ src/main/java/my/photomanager/v1/service/ConfigurationService.java
@@ -1,12 +1,14 @@
 package my.photomanager.v1.service;
 
-import com.google.common.collect.Lists;
 import java.util.List;
+import static net.logstash.logback.argument.StructuredArguments.kv;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import my.photomanager.v1.model.Configuration;
+import my.photomanager.v1.repository.ConfigurationRepository;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,12 +16,21 @@ import org.springframework.stereotype.Service;
 @Log4j2
 public class ConfigurationService {
 
+	private final ConfigurationRepository repository;
+
 	public List<Configuration> getConfigurations() {
-		return Lists.newArrayList();
+		return repository.findAll();
 	}
 
 	public Configuration saveConfiguration(@NonNull Configuration configuration) {
-		return null;
+		repository.findByFolderPathAndIndexInterval(
+				configuration.getFolderPath(), configuration.getUpdateInterval())
+				.ifPresent(config -> new InternalError(""));
+
+		var savedConfiguration = repository.saveAndFlush(configuration);
+		log.info("saved {}", kv("configuration", savedConfiguration));
+
+		return savedConfiguration;
 	}
 
 	public Configuration updateConfiguration(long ID, @NonNull String folderPath, @NonNull String updateInterval) {
