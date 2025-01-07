de710f03689613e781fbe9d9fd4a0eb402233a7e
Sebastian Büge <sebastian_buege@gmx.de>
2025-01-07 12:22:07 +0100

    add comments

--- src/main/java/my/photomanager/v1/service/ConfigurationService.java
+++ src/main/java/my/photomanager/v1/service/ConfigurationService.java
@@ -14,19 +14,45 @@ import org.springframework.stereotype.Service;
 @Log4j2
 public class ConfigurationService {
 
+	/**
+	 * @return a list with all configuration objects
+	 */
 	public List<Configuration> getConfigurations() {
 		return Lists.newArrayList();
 	}
 
+	/**
+	 * save a new configuration object
+	 * check if the configuration exists already
+	 * 
+	 * @param configuration the configuration
+	 * @return the saved configuration
+	 */
 	public Configuration saveConfiguration(@NonNull Configuration configuration) {
+		// TODO save configuration
 		return null;
 	}
 
-	public Configuration updateConfiguration(long ID, @NonNull String folderPath, @NonNull String updateInterval) {
+	/**
+	 * update an existing configuration object
+	 * 
+	 * @param ID            the existing configuration id
+	 * @param folderPath    the new folder path
+	 * @param indexInterval the new index interval
+	 * @return the updated configuration
+	 */
+	public Configuration updateConfiguration(long ID, @NonNull String folderPath, @NonNull String indexInterval) {
+		// TODO update configuration
 		return null;
 	}
 
+	/**
+	 * delete an existing configuration
+	 * 
+	 * @param ID the existing configuration id
+	 */
 	public void deleteConfiguration(long ID) {
+		// TODO delete configuration
 
 	}
 }
