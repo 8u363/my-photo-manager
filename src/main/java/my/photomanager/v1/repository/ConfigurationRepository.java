b1f15585d86a5bcce8239dd906c24e002cb5be8e
Sebastian Büge <sebastian_buege@gmx.de>
2025-01-07 13:37:55 +0100

    update configuration repository

--- src/main/java/my/photomanager/v1/repository/ConfigurationRepository.java
+++ src/main/java/my/photomanager/v1/repository/ConfigurationRepository.java
@@ -1,5 +1,14 @@
 package my.photomanager.v1.repository;
 
-public interface ConfigurationRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import lombok.NonNull;
+import my.photomanager.v1.model.Configuration;
+
+public interface ConfigurationRepository extends JpaRepository<Configuration, Long> {
+
+    Optional<Configuration> findByFolderPathAndIndexInterval(@NonNull String folderPath,
+            @NonNull String indexInterval);
 }
