c3cf7d950e860aac8e3cbdcf520bd7413d5fbf84
Sebastian Büge <sebastian_buege@gmx.de>
2025-04-08 10:09:19 +0200

    cleanup test utils

--- src/test/java/my/photomanager/TestUtils.java
+++ src/test/java/my/photomanager/TestUtils.java
@@ -2,6 +2,7 @@ package my.photomanager;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.UUID;
 import my.photomanager.photo.Photo;
 
@@ -13,56 +14,19 @@ public class TestUtils {
         public static final long PHOTO_ID = 1L;
         public static final String PHOTO_HASH_VALUE = "1234567890";
         public static final String PHOTO_FILE_PATH = "testPhoto.jpg";
-        public static final int PHOTO_HEIGHT = 768;
-        public static final int PHOTO_WIDTH = 1024;
+        public static final int PHOTO_HEIGHT_768 = 768;
+        public static final int PHOTO_WIDTH_1024 = 1024;
+        public static final LocalDate PHOTO_CREATION_DATE = LocalDate.of(2025, Month.JANUARY, 1);
 
-        public static final LocalDate PHOTO_CREATION_DATE_DECEMBER_2024 =
-                        LocalDate.of(2024, Month.DECEMBER, 24);
-        public static final LocalDate PHOTO_CREATION_DATE_DECEMBER_2025 =
-                        LocalDate.of(2025, Month.DECEMBER, 24);
-        public static final LocalDate PHOTO_CREATION_DATE_APRIL_2025 =
-                        LocalDate.of(2025, Month.APRIL, 1);
-
-        public static final String PHOTO_COUNTRY_GERMANY = "Deutschland";
-        public static final String PHOTO_CITY_BERLIN = "Berlin";
-        public static final String PHOTO_CITY_HAMBURG = "Hamburg";
-        public static final String PHOTO_COUNTRY_FRANCE = "Frankreich";
-        public static final String PHOTO_CITY_PARIS = "Paris";
-
-        public static final Photo buildDefaultPhoto() {
-                return Photo.builder(PHOTO_FILE_PATH, PHOTO_HASH_VALUE).withHeight(PHOTO_HEIGHT)
-                                .withWidth(PHOTO_WIDTH).build();
-        }
-
-        public static final Photo buildPhotoWithRandomUUID() {
-                return Photo.builder(PHOTO_FILE_PATH, UUID.randomUUID().toString())
-                                .withHeight(PHOTO_HEIGHT).withWidth(PHOTO_WIDTH).build();
-        }
-
-        public static final Photo buildPhoto(String hashValue, LocalDate creationDate) {
-                return buildPhoto(hashValue, creationDate, null, null);
-        }
-
-        public static final Photo buildPhoto(String hashValue, String country, String city) {
-                return buildPhoto(hashValue, null, country, city);
+        public static Photo buildDefaultPhoto() {
+                return buildPhoto(PHOTO_HASH_VALUE, PHOTO_CREATION_DATE, PHOTO_WIDTH_1024,
+                                PHOTO_HEIGHT_768, PHOTO_HASH_VALUE, PHOTO_FILE_PATH);
         }
 
         public static final Photo buildPhoto(String hashValue, LocalDate creationDate,
-                        String country, String city) {
-                var photo = buildDefaultPhoto().toBuilder().withHashValue(hashValue).build();
-
-                if (creationDate != null) {
-                        photo = photo.toBuilder().withCreationDate(creationDate).build();
-                }
-
-                if (country != null) {
-                        photo = photo.toBuilder().withCountry(country).build();
-                }
-
-                if (city != null) {
-                        photo = photo.toBuilder().withCity(city).build();
-                }
-
-                return photo;
+                        int photoHeight, int photoWidth, String country, String city) {
+                return Photo.builder(PHOTO_FILE_PATH, hashValue).withCreationDate(creationDate)
+                                .withHeight(photoHeight).withWidth(photoWidth).withCountry(country)
+                                .withCity(city).build();
         }
 }
