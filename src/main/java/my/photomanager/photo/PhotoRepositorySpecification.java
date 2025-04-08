5a5298fd005e8b52a0475f67949e51d1a4a23c1d
Sebastian Büge <sebastian_buege@gmx.de>
2025-04-08 10:09:06 +0200

    update specification

--- src/main/java/my/photomanager/photo/PhotoRepositorySpecification.java
+++ src/main/java/my/photomanager/photo/PhotoRepositorySpecification.java
@@ -8,37 +8,92 @@ import org.springframework.data.jpa.domain.Specification;
 import jakarta.persistence.criteria.Expression;
 import jakarta.persistence.criteria.Predicate;
 
-public class PhotoReposiorySpecification {
+
+public class PhotoRepositorySpecification {
 
     public static Specification<Photo> findBy(String country, String city, Integer year,
-            Month month) {
+            Month month, Orientation orientation) {
         return (root, query, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
 
-            if (country != null) {
-                predicates.add(criteriaBuilder.equal(root.get("country"), country));
-            }
-
-            if (city != null) {
-                predicates.add(criteriaBuilder.equal(root.get("city"), city));
-            }
-
-            if (year != null) {
-                var startDate = LocalDate.of(year, Month.JANUARY, 1);
-                var endDate = LocalDate.of(year, Month.DECEMBER, 31);
-                predicates
-                        .add(criteriaBuilder.between(root.get("creationDate"), startDate, endDate));
-            }
-
-            if (month != null) {
-                Expression<Integer> creationDateMonth =
-                        criteriaBuilder.function("MONTH", Integer.class, root.get("creationDate"));
-
-                predicates.add(criteriaBuilder.equal(creationDateMonth, month.getValue()));
-            }
+            predicates.add(findByCountry(country).toPredicate(root, query, criteriaBuilder));
+            predicates.add(findByCity(city).toPredicate(root, query, criteriaBuilder));
+            predicates.add(findByCreationYear(year).toPredicate(root, query, criteriaBuilder));
+            predicates.add(findByCreationMonth(month).toPredicate(root, query, criteriaBuilder));
+            predicates
+                    .add(findByOrientation(orientation).toPredicate(root, query, criteriaBuilder));
 
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
     }
 
+    public static Specification<Photo> findByCountry(String country) {
+        return (root, query, criteriaBuilder) -> {
+            if (country == null) {
+                return criteriaBuilder.conjunction();
+            }
+
+            return criteriaBuilder.equal(root.get("country"), country);
+        };
+    }
+
+
+    public static Specification<Photo> findByCity(String city) {
+        return (root, query, criteriaBuilder) -> {
+            if (city == null) {
+                return criteriaBuilder.conjunction();
+            }
+
+            return criteriaBuilder.equal(root.get("city"), city);
+        };
+    }
+
+    public static Specification<Photo> findByCreationYear(Integer year) {
+        return (root, query, criteriaBuilder) -> {
+            if (year == null) {
+                return criteriaBuilder.conjunction();
+            }
+
+            Expression<Integer> creationYear =
+                    criteriaBuilder.function("YEAR", Integer.class, root.get("creationDate"));
+            return criteriaBuilder.equal(creationYear, year);
+        };
+    }
+
+    public static Specification<Photo> findByCreationMonth(Month month) {
+        return (root, query, criteriaBuilder) -> {
+            if (month == null) {
+                return criteriaBuilder.conjunction();
+            }
+
+            Expression<Integer> creationMonth =
+                    criteriaBuilder.function("MONTH", Integer.class, root.get("creationDate"));
+            return criteriaBuilder.equal(creationMonth, month.getValue());
+        };
+    }
+
+    public static Specification<Photo> findByOrientation(Orientation orientation) {
+        return (root, query, criteriaBuilder) -> {
+            if (orientation == null) {
+                return criteriaBuilder.conjunction();
+            }
+
+            Expression<Integer> width = root.get("width");
+            Expression<Integer> height = root.get("height");
+
+            switch (orientation) {
+                case LANDSCAPE:
+                    return criteriaBuilder.greaterThan(width, height);
+
+                case PORTRAIT:
+                    return criteriaBuilder.greaterThan(height, width);
+
+                case SQUARE:
+                    return criteriaBuilder.equal(height, width);
+
+                default:
+                    return criteriaBuilder.conjunction();
+            }
+        };
+    }
 }
